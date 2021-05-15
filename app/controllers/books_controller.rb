d67700d30e4bcd4885bf96e893f634d88286233b
Masanko <[dwrys431@gmail.com]>
2021-05-15 13:43:52 +0000

    index 途中

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,13 @@
 class BooksController < ApplicationController
   def show
+    @book = Book.find(params[:id])
   end
  
   def index
     @books = Book.all
     @book = Book.new
+    
+   
   end
 
   def new
