8515a07fddc44c659a5900debb19d35cb24d3aa2
Masanko <[dwrys431@gmail.com]>
2021-05-17 14:22:29 +0000

    バリテーションのみ

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,8 +6,6 @@ class BooksController < ApplicationController
   def index
     @books = Book.all
     @book = Book.new
-    
-    
    
   end
 
@@ -17,8 +15,12 @@ class BooksController < ApplicationController
   
   def create
     book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+   
+    if book.save
+      flash[:notice] = "book was successfully created"
+      redirect_to book_path(book.id)
+ 
+    end
   end
   
   def edit
@@ -27,13 +29,17 @@ class BooksController < ApplicationController
   
   def update
     book = Book.find(params[:id])
-    book.update(book_params)
+   if  book.update(book_params)
+    flash[:notice] = "book was successfully update."
     redirect_to book_path(book.id)
+  
   end
+end
   
   def destroy
     book = Book.find(params[:id])
     book.destroy
+    flash[:notice] = "Book was successfully destroyed."
     redirect_to books_path
   end
   
